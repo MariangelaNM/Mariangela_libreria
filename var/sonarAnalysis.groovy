f8e6fc15682a262cd66b481849ed7befc92c5b96
MariangelaNM <91926755+MariangelaNM@users.noreply.github.com>
2023-10-21 16:15:45 -0600

    Update sonarAnalysis.groovy

--- var/sonarAnalysis.groovy
+++ var/sonarAnalysis.groovy
@@ -1,36 +1,29 @@
-pipeline {
-    agent any
-    
-    stages {
-        stage('Checkout') {
-            steps {
-                // Configuración para realizar la checkout de tu repositorio (por ejemplo, usando Git).
-               git credentialsId: 'MN_Token', url: 'https://github.com/MariangelaNM/Mariangela_libreria'      }
+def call(boolean abortOnFailure = false, boolean abortPipeline = false) {
+    // Ejecuta el análisis de SonarQube
+      environment {
+                SCANNER_HOME = tool 'SonarScanner'
             }
-        }
-        
-        stage('SonarQube Analysis') {
-            steps {
-                // Carga el script sonarAnalysis.groovy y llama a la función.
-                script {
-                    def sonarAnalysisScript = load 'vars/sonarAnalysis.groovy'
-                    sonarAnalysisScript()
+         
+                withSonarQubeEnv(installationName: 'Sonar Local',credentialsId: 'sonar-token') {
+                    sh '${SCANNER_HOME}/bin/sonar-scanner -Dsonar.projectKey=threepoints_devops_webserver -Dsonar.projectName=threepoints_devops_webserver'
+                }   
+             
+
+    // Espera 5 minutos con un timeout
+    timeout(time: 5, unit: 'MINUTES') {
+        // Comprueba el resultado del QualityGate de SonarQube
+        script {
+            def qualityGateStatus = waitForQualityGate()
+            if (qualityGateStatus != 'OK' && abortOnFailure) {
+                error("QualityGate failed.")
+                if (abortPipeline) {
+                    currentBuild.result = 'FAILURE'
+                    error("Aborting the pipeline.")
+                    return
                 }
-            }
-        }
-
-        stage('Build') {
-            steps {
-                // Acciones para compilar tu proyecto.
-                sh 'mvn clean install'
-            }
-        }
-
-        stage('Deploy') {
-            steps {
-                // Acciones para desplegar tu proyecto en un entorno específico.
-                sh 'deploy-script.sh'
             }
         }
     }
 }
+
+return this
