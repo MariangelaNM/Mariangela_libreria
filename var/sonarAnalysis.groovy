8f130ac0853607a80db62e7e3f81fec079caa771
MariangelaNM <91926755+MariangelaNM@users.noreply.github.com>
2023-10-21 18:27:45 -0600

    Update sonarAnalysis.groovy

--- var/sonarAnalysis.groovy
+++ var/sonarAnalysis.groovy
@@ -2,19 +2,16 @@ def call(projectKey, gitBranch, abortPipeline = false) {
     def scannerResult = 1
     def haveToExitPipeline = false
 
-    node {
-        stage('SonarQube Analysis') {
-            if (isUnix()) {
-                scannerResult = sh(script: "sonar-scanner -Dsonar.projectKey=${projectKey} -Dsonar.sources=.", returnStatus: true)
-            } else {
-                scannerResult = bat(script: "sonar-scanner -Dsonar.projectKey=${projectKey} -Dsonar.sources=.", returnStatus: true)
-            }
+    timeout(time: 30, unit: 'SECONDS') {
+         withSonarQubeEnv(installationName: 'Sonar Local',credentialsId: 'sonar-token') {
+            scannerResult = bat(script: "sonar-scanner -Dsonar.projectKey=${projectKey} -Dsonar.sources=.", returnStatus: true)
         }
     }
 
     if (abortPipeline && scannerResult != 0) {
         haveToExitPipeline = true
     } else if (!abortPipeline) {
+        // Verificar si abortar el pipeline segun nombre de la rama gitBranch
         if (gitBranch == 'master' || gitBranch.startsWith('hotfix')) {
             haveToExitPipeline = true
         }
